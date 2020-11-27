323dbc0ccf00dc9decc0cef10f06cab976c72412
PUYUP <hellopuyup@gmail.com>
2020-11-27 14:50:08 +0700

    Add duration

--- apps/training/api/instructor/course/v1/views.py
+++ apps/training/api/instructor/course/v1/views.py
@@ -1,10 +1,10 @@
-from django.core.exceptions import ValidationError
+from django.core.exceptions import ObjectDoesNotExist, ValidationError
 from django.db import transaction
 from django.utils.decorators import method_decorator
 from django.views.decorators.cache import never_cache
 
 from rest_framework import viewsets, status as response_status
-from rest_framework.exceptions import NotAcceptable
+from rest_framework.exceptions import NotAcceptable, NotFound
 from rest_framework.permissions import IsAdminUser, IsAuthenticated
 from rest_framework.response import Response
 
@@ -89,3 +89,18 @@ class CourseQuizApiView(viewsets.ViewSet):
 
             return Response(serializer.data, status=response_status.HTTP_200_OK)
         return Response(serializer.errors, status=response_status.HTTP_406_NOT_ACCEPTABLE)
+
+    @method_decorator(never_cache)
+    @transaction.atomic
+    def partial_update(self, request, uuid=None, format=None):
+        context = {'request': request}
+        try:
+            queryset = self.queryset().select_for_update().get(uuid=uuid)
+        except ObjectDoesNotExist:
+            raise NotFound()
+
+        serializer = CourseQuizSerializer(queryset, data=request.data, partial=True, context=context)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data, status=response_status.HTTP_200_OK)
+        return Response(serializer.errors, status=response_status.HTTP_400_BAD_REQUEST)
