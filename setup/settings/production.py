482d29f3b392757f221ccab9d0d852bbc84afa05
PUYUP <hellopuyup@gmail.com>
2020-11-27 13:57:38 +0700

    Fix password

--- setup/settings/production.py
+++ setup/settings/production.py
@@ -125,7 +125,6 @@ CACHES['default']['LOCATION'] = REDIS_URL
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
