26122cec460e0f73cea5bbc07c77f1ea9ea3a1ed
PUYUP <hellopuyup@gmail.com>
2020-11-27 13:56:32 +0700

    Password length to 6

--- setup/settings/production.py
+++ setup/settings/production.py
@@ -130,7 +130,7 @@ AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
         'OPTIONS': {
-            'min_length': 4,
+            'min_length': 6,
         }
     },
 ]
