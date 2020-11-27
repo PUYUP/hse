d6ffdafe776cf790fafa30a2d30edd88209a7d77
PUYUP <hellopuyup@gmail.com>
2020-11-27 20:28:46 +0700

    Test

--- setup/settings/production.py
+++ setup/settings/production.py
@@ -37,7 +37,6 @@ REDIS_URL = 'redis://' + REDIS_HOST + ':' + REDIS_PORT + '/0'
 # https://docs.djangoproject.com/en/2.2/ref/settings/
 SESSION_COOKIE_SECURE = False
 SESSION_COOKIE_HTTPONLY = False
-
 SECURE_REFERRER_POLICY = 'same-origin'
 SECURE_CONTENT_TYPE_NOSNIFF = True
 SECURE_BROWSER_XSS_FILTER = True
