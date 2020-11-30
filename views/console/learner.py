18353a345e18181c95be5e993d965b45947667d3
PUYUP <hellopuyup@gmail.com>
2020-11-30 09:26:15 +0700

    Datatables

--- views/console/learner.py
+++ views/console/learner.py
@@ -3,6 +3,7 @@ from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.views import View
 from django.shortcuts import render
 from django.conf import settings
+from django.db.models import Count, Q
 
 from utils.generals import get_model
 from utils.pagination import Pagination
@@ -16,8 +17,17 @@ class LearnerView(View):
 
     def get(self, request):
         queryset = User.objects.prefetch_related('account', 'profile') \
-            .select_related('account', 'profile') \
-            .filter(is_staff=False)
+            .annotate(
+                enroll_count=Count('enroll', distinct=True),
+                simulation_count=Count('simulation', distinct=True),
+                answer_count=Count('simulation__answer', distinct=True),
+                answer_count_survey=Count(
+                    'simulation__answer', 
+                    distinct=True,
+                    filter=Q(simulation__answer__is_true=True) & Q(simulation__answer__course_quiz__position='survey')
+                )
+            ) \
+            .select_related('account', 'profile')
         
         page_num = int(self.request.GET.get('p', 0))
         paginator = Paginator(queryset, settings.PAGINATION_PER_PAGE)
