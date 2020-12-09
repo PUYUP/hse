2824a946043cfa9aaac355b01d8bed3559406715
PUYUP <hellopuyup@gmail.com>
2020-12-09 15:29:18 +0700

    Change to dashboard

--- views/urls.py
+++ views/urls.py
@@ -7,13 +7,13 @@ from .general import HomeView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
-    path('console/', ConsoleView.as_view(), name='home'),
-    path('console/learner/', LearnerView.as_view(), name='learner'),
-    path('console/learner/<uuid:uuid>/', LearnerDetailView.as_view(), name='learner_detail'),
-    path('console/course/', CourseView.as_view(), name='course'),
-    path('console/course/editor/', CourseEditorView.as_view(), name='course_editor'),
-    path('console/course/<uuid:uuid>/', CourseDetailView.as_view(), name='course_detail'),
-    path('console/course/<uuid:uuid>/editor/', CourseEditorView.as_view(), name='course_editor'),
-    path('console/course/<uuid:uuid>/quiz/', CourseQuizView.as_view(), name='course_quiz'),
-    path('console/chapter/<uuid:uuid>/editor/', ChapterView.as_view(), name='chapter_editor'),
+    path('dashboard/', ConsoleView.as_view(), name='home'),
+    path('dashboard/learner/', LearnerView.as_view(), name='learner'),
+    path('dashboard/learner/<uuid:uuid>/', LearnerDetailView.as_view(), name='learner_detail'),
+    path('dashboard/course/', CourseView.as_view(), name='course'),
+    path('dashboard/course/editor/', CourseEditorView.as_view(), name='course_editor'),
+    path('dashboard/course/<uuid:uuid>/', CourseDetailView.as_view(), name='course_detail'),
+    path('dashboard/course/<uuid:uuid>/editor/', CourseEditorView.as_view(), name='course_editor'),
+    path('dashboard/course/<uuid:uuid>/quiz/', CourseQuizView.as_view(), name='course_quiz'),
+    path('dashboard/chapter/<uuid:uuid>/editor/', ChapterView.as_view(), name='chapter_editor'),
 ]
